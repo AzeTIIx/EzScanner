60dc91539000a1d8ed2599e6333a716437cc20a5
Charles AIMIN <93607991+CharlesAIMIN@users.noreply.github.com>
2022-10-13 13:02:35 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='PyPentest',
+    version="0.1",
+    packages=find_packages(),
+    author="CharlesAIMIN",
+    author_email="caimin.ing2024@esaip.org",
+    install_requires=["pystyle","nmap","sys"],
+    description="PyPentest allow you to perform an NMAP scan on a network",
+    include_package_data=True,
+    url='http://github.com/CharlesAIMIN/PyPentest',
+    classifiers=[
+        "Programming Language :: Python",
+    ],
+)
