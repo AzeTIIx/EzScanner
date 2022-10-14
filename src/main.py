9bafbf54e912b248cf8ca782d6f25fa1c46b379c
Charles AIMIN <93607991+CharlesAIMIN@users.noreply.github.com>
2022-10-14 16:39:45 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -22,15 +22,19 @@ def params():
     return begin, end, target
 
 def scan(begin, end, target):
-    scanner = nmap.PortScanner()
+    o = []
 
     for i in trange(int(begin), int(end)+1):
-        res = scanner.scan(target,str(i))
+        res = nmap.PortScanner().scan(target,str(i))
         res = res['scan'][target]['tcp'][i]['state']
         
-    for i in range(int(begin), int(end)+1):
         if res == "open":
-            print(f'port {i} is {res}.')
+            o.append(i)
+        
+    for i in range(len(o)):
+        print("Port {} is open" . format(o[i]))
+    
+    
     
 
 if __name__ =='__main__':
@@ -41,10 +45,10 @@ if __name__ =='__main__':
         print("     main.py [first port] [last port] [target ip]\n")
         
     elif (n < 3):
-        print("Error, synthax is main.py  [first port] [last port] [target]")
+        print("Error, synthax is main.py [first port] [last port] [target]")
         
     else:
         title()
         begin, end, target = params()
-        res = scan(begin, end, target)
+        scan(begin, end, target)
 
