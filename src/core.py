68e6d8936558d76490a0381cba25d71e6cfbe852
Charles AIMIN <93607991+CharlesAIMIN@users.noreply.github.com>
2022-10-17 16:07:32 +0200

    Update core.py

--- src/core.py
+++ src/core.py
@@ -1,7 +1,7 @@
 from platform import platform
 from pystyle import *
 from tqdm import trange
-import nmap, sys, os, platform
+import socket, sys, os, platform
 
 
 def title():
@@ -19,27 +19,30 @@ def title():
 def params():
     begin = sys.argv[2]
     end = sys.argv[3]
-    target = sys.argv[4]
-    return begin, end, target
+    ip = sys.argv[4]
+    return begin, end, ip
 
-def scan(begin, end, target):
+def scan(begin, end, ip):
     o = []
 
-    for i in trange(int(begin), int(end)+1):
-        res = nmap.PortScanner().scan(target,str(i))
-        res = res['scan'][target]['tcp'][i]['state']
-        
-        if res == "open":
-            o.append(i)
-            
+    for port in trange(int(begin), int(end)+1):
+        try:
+            r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            r.settimeout(0.2)
+            r.connect((ip, port))
+            r.settimeout(None)
+            r.close()
+            o.append(port)
+        except:
+            pass
+
+
     if len(o) < 1 :
         print(Colorate.Color(Colors.red,"Every port in the range are closed or filtered", True))
     else:
         for i in range(len(o)):
-            print("Port {} is open" . format(o[i]))
-    
-    
-        
+            print(Colorate.Color(Colors.green,"Port {} is open" . format(o[i]), True))
+
 def isup(host):
     os_system = platform.system()
     if(os_system=="Linux"):
@@ -48,10 +51,10 @@ def isup(host):
     else:
         cmd = str("ping " + host)
         os.system(cmd)
-        
+
 def main():
     n = len(sys.argv)
-    
+
     if(sys.argv[1] == "--help"):
         title()
         print("Usage : \n")
@@ -62,14 +65,14 @@ def main():
     elif (sys.argv[1] == "--isup"):
         title()
         host = sys.argv[2]
-        isup(host)    
-    
+        isup(host)
+
     elif (n < 3):
         error = "Error, synthax is main.py [first port] [last port] [target]"
         print(Colorate.Color(Colors.red, error, True))
-        
+
     elif(sys.argv[1] == '--nmap'):
         title()
-        begin, end, target = params()
-        scan(begin, end, target)
+        begin, end, ip = params()
+        scan(begin, end, ip)
 
