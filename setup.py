0c3684256447d31b677dceda4d5b8dc69774a0d4
Charles AIMIN <93607991+CharlesAIMIN@users.noreply.github.com>
2022-10-17 16:08:06 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PyPentest',
-    version="0.4",
+    version="0.5",
     packages=find_packages(),
     author="CharlesAIMIN",
     author_email="caimin.ing2024@esaip.org",
