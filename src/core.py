f890252ae51c9008ff71d44e348045d60ab01f4b
Charles AIMIN <93607991+CharlesAIMIN@users.noreply.github.com>
2022-10-24 16:13:57 +0200

    Update core.py

--- src/core.py
+++ src/core.py
@@ -1,7 +1,6 @@
-from platform import platform
 from pystyle import *
 from tqdm import trange
-import socket, sys, os, platform, argparse
+import socket, argparse, subprocess
 
 
 def title():
@@ -25,6 +24,7 @@ def scan(begin, end, ip):
             r = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             r.settimeout(0.2)
             r.connect((ip, port))
+            banner = r.recv(1024).decode()
             r.settimeout(None)
             r.close()
             o.append(port)
@@ -37,41 +37,69 @@ def scan(begin, end, ip):
     else:
         for i in range(len(o)):
             print(Colorate.Color(Colors.green,"Port {} is open" . format(o[i]), True))
+        print ('[+] {}' .format(str(banner))) 
+    return o
 
-def isup(host):
-    os_system = platform.system()
-    if(os_system=="Linux"):
-        cmd = str("ping -c 4 " + host)
-        os.system(cmd)
-    else:
-        cmd = str("ping " + host)
-        os.system(cmd)
+
+    
+def rangescan(begin, end, ip2):
+    for ping in range(begin,end):
+        address = ip2 + str(ping)
+        res = subprocess.call(['ping', '-c', '1', '-w', '1',  address], stdout=subprocess.DEVNULL,stderr=subprocess.STDOUT)
+        
+        if res == 0:
+            print(Colorate.Color(Colors.green,"{} is up" .format(address), True))
+        elif res == 2:
+            print("no response from", address)
+        else:
+            print(Colorate.Color(Colors.red,"ping to {} is closed" .format(address), True))    
+    
+
+
+
 
 def main():
     parser = argparse.ArgumentParser()
     subparser = parser.add_subparsers(dest='command')
     
-    nmap = subparser.add_parser('nmap')
-    hostup = subparser.add_parser('isup')
+    nmap = subparser.add_parser('nmap', help='Perform a NMAP scan on a target')
+    hostup = subparser.add_parser('isup', help='List all up hosts on a range')
 
-    nmap.add_argument('-b', type=int)
-    nmap.add_argument('-e', type=int)
-    nmap.add_argument('-target')
+    nmap.add_argument('-b', type=int, help='First port to be scanned in the range')
+    nmap.add_argument('-e', type=int, help='Last port to be scanned in the range')
+    nmap.add_argument('-ip', help='IP to scan')
     
-    hostup.add_argument('-ip')
+
+    hostup.add_argument('-b', type=int, help='First port to be scanned in the range')
+    hostup.add_argument('-e', type=int, help='Last port to be scanned in the range')
+    hostup.add_argument('-ip', help='IP to scan')
+
 
     args = parser.parse_args()
+    
+    
 
     if args.command == 'nmap':
         title()
         begin = args.b
         end =args.e
-        ip = args.target
-        scan(begin, end, ip)
+        ip = args.ip
+
+        o = []
+        o.append(scan(begin, end, ip))
         
+        
+    
     elif args.command == 'isup':
         title()
+        begin = args.b
+        end =args.e
         ip = args.ip
-        isup(ip)
-    
+        net1 = ip.split('.')
+        a='.'
+        ip2 = net1[0] + a + net1[1] + a + net1[2] + a
+        end = end + 1
+        rangescan(begin, end, ip2)
+        
+ 
         
